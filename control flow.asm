4183c6e4edfc35a73ee22bfe0859c26a892c5135
Dionnn-max <dchen7684@sdsu.edu>
2025-12-10 23:07:34 -0800

    Update control flow.asm

--- control flow.asm
+++ control flow.asm
@@ -1,30 +1,20 @@
-# control_flow.asm
-# Shows branching with Disney theme
+# Shows loops using Disney-themed concepts
 
 .data
   counter: .word 0
   limit: .word 5
 
 .text
-  # DISNEY-8 concept loop:
-  # loop:
-  #   PIRATESOFCARIBBEAN $c1, counter
-  #   PIRATESOFCARIBBEAN $c2, limit
-  #   SPLASHMOUNTAIN $c3, $c1, 1     # Increment counter
-  #   HAUNTEDMANSION $c3, counter
-  #   JUNGLE_CRUISE $c3, $c2, done   # Branch if counter == limit
-  #   MAGICCARPET loop               # Jump back
-  # done:
-  #   GOODNIGHTKISS
+main:
+  lw $t0, counter
+  lw $t1, limit
   
-  # MIPS equivalent:
-  main:
-    lw $t0, counter
-    lw $t1, limit
-  loop:
-    add $t0, $t0, 1
-    sw $t0, counter
-    bne $t0, $t1, loop   # Like JUNGLE_CRUISE
-  done:
-    li $v0, 10
-    syscall
+loop:
+  add $t0, $t0, 1          # Like SPLASHMOUNTAIN with 1
+  sw $t0, counter
+  
+  bne $t0, $t1, loop       # JUNGLE_CRUISE: keep going if not at limit
+  
+  # When counter == limit, we're done!
+  li $v0, 10               # GOODNIGHTKISS
+  syscall
